f68f099d2a0d4c7c0c0ca8a0ec76c927d378d15b
CNexus <cnexus0@gmail.com>
2013-09-15 10:17:03 -0700

    Add d2spr
    Add d2spr device
    
    Add d2spr

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/a510.mk \
     $(LOCAL_DIR)/d2att.mk \
+    $(LOCAL_DIR)/d2spr.mk \
     $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2usc.mk \
     $(LOCAL_DIR)/d2vzw.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,7 @@ add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_tilapia-userdebug
 add_lunch_combo aokp_manta-userdebug
 add_lunch_combo aokp_d2att-userdebug
+add_lunch_combo aokp_d2spr-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2usc-userdebug
 add_lunch_combo aokp_d2vzw-userdebug
